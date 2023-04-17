b424dd48e11f2fd47993ce16bc215191ccdba0a5
Davis198 <Davis198>
2023-04-17 04:39:36 -0700

    shell

--- main.c
+++ main.c
@@ -2,13 +2,13 @@
 
 /**
  * main - entry point
- * @ab: arg count
- * @ay: arg vector
+ * @ac: arg count
+ * @av: arg vector
  *
  * Return: 0 on success, 1 on error
  */
 
-int main(int ab, char **ay)
+int main(int ac, char **av)
 {
 	info_t info[] = { INFO_INIT };
 	int fd = 2;
@@ -18,18 +18,18 @@ int main(int ab, char **ay)
 			: "=r" (fd)
 			: "r" (fd));
 
-	if (ab == 2)
+	if (ac == 2)
 	{
-		fd = open(ay[1], O_RDONLY);
+		fd = open(av[1], O_RDONLY);
 		if (fd == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
 			if (errno == ENOENT)
 			{
-				_eputs(ay[0]);
+				_eputs(av[0]);
 				_eputs(": 0: Can't open ");
-				_eputs(ay[1]);
+				_eputs(av[1]);
 				_eputchar('\n');
 				_eputchar(BUF_FLUSH);
 				exit(127);
@@ -40,6 +40,6 @@ int main(int ab, char **ay)
 	}
 	populate_env_list(info);
 	read_history(info);
-	hsh(info, ay);
+	hsh(info, av);
 	return (EXIT_SUCCESS);
 }
